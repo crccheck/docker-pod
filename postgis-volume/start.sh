3662d45cf99ca6ce07295c08101cdb02f2a75aa7
crccheck <c@crccheck.com>
2014-08-26 23:58:46 -0500

    start updating my old postgis-volume container

--- postgis-volume/start.sh
+++ postgis-volume/start.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # strongly influenced by:
 # https://github.com/orchardup/docker-postgresql/blob/master/run
 
 set -e
 
-POSTGRESQL_BIN=/usr/lib/postgresql/9.2/bin/postgres
-POSTGRESQL_CONFIG_FILE=/etc/postgresql/9.2/main/postgresql.conf
-POSTGRESQL_DATA=/mnt/postgres/9.2
+POSTGRESQL_BIN=/usr/lib/postgresql/9.3/bin/postgres
+POSTGRESQL_CONFIG_FILE=/etc/postgresql/9.3/main/postgresql.conf
+POSTGRESQL_DATA=/var/lib/postgresql
 
+# need to check this in case the user is using an external volume
 if [ ! -d $POSTGRESQL_DATA ]; then
     mkdir -p $POSTGRESQL_DATA
     chown -R postgres:postgres $POSTGRESQL_DATA/..
-    su postgres -c "/usr/lib/postgresql/9.2/bin/initdb -D $POSTGRESQL_DATA"
+    /usr/lib/postgresql/9.3/bin/initdb -D $POSTGRESQL_DATA
 fi
 
-
-su postgres -c "$POSTGRESQL_BIN --single --config-file=$POSTGRESQL_CONFIG_FILE <<< \"CREATE USER docker WITH SUPERUSER;\""
-su postgres -c "$POSTGRESQL_BIN --config-file=$POSTGRESQL_CONFIG_FILE"
+$POSTGRESQL_BIN --single --config-file=$POSTGRESQL_CONFIG_FILE <<< \"CREATE USER docker WITH SUPERUSER;\"
+$POSTGRESQL_BIN --config-file=$POSTGRESQL_CONFIG_FILE
