7a1c1212d168b61830da9c70a1cf6ddc9b89e869
crccheck <c@crccheck.com>
2014-08-27 01:17:42 -0500

    get volume and config working again

--- postgis-volume/start.sh
+++ postgis-volume/start.sh
@@ -7,14 +7,16 @@ set -e
 
 POSTGRESQL_BIN=/usr/lib/postgresql/9.3/bin/postgres
 POSTGRESQL_CONFIG_FILE=/etc/postgresql/9.3/main/postgresql.conf
-POSTGRESQL_DATA=/var/lib/postgresql
+# data_directory
+POSTGRESQL_DATA=/data/postgresql
 
-# need to check this in case the user is using an external volume
+
 if [ ! -d $POSTGRESQL_DATA ]; then
     mkdir -p $POSTGRESQL_DATA
-    chown -R postgres:postgres $POSTGRESQL_DATA/..
-    /usr/lib/postgresql/9.3/bin/initdb -D $POSTGRESQL_DATA
+    chown -R postgres:postgres $POSTGRESQL_DATA
+    su postgres -c "/usr/lib/postgresql/9.3/bin/initdb -D $POSTGRESQL_DATA"
 fi
 
-$POSTGRESQL_BIN --single --config-file=$POSTGRESQL_CONFIG_FILE <<< \"CREATE USER docker WITH SUPERUSER;\"
-$POSTGRESQL_BIN --config-file=$POSTGRESQL_CONFIG_FILE
+su postgres -c "$POSTGRESQL_BIN --single --config-file=$POSTGRESQL_CONFIG_FILE <<< \"CREATE USER docker WITH SUPERUSER;\""
+su postgres -c "$POSTGRESQL_BIN --single --config-file=$POSTGRESQL_CONFIG_FILE <<< \"CREATE DATABASE docker WITH OWNER=docker;\""
+su postgres -c "$POSTGRESQL_BIN --config-file=$POSTGRESQL_CONFIG_FILE"
