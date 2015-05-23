6695c2107baf9a0c2d6af8f49061dcc7f4c3cfe6
crccheck <c@crccheck.com>
2015-05-22 20:41:30 -0500

    limit count output to once a second

--- logstash-redis-spewer/spew.sh
+++ logstash-redis-spewer/spew.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-MESSAGE=hi
+set -e
 
 LIST_NAME=${LIST_NAME:-logstash}
 
 WORD_SOURCE=/usr/share/dict/words
 
 while read line; do
-    redis-cli LPUSH ${LIST_NAME} $line
-    sleep 1
+  count=$(redis-cli LPUSH ${LIST_NAME} $line)
+  timestamp=$(date +%S)
+  if [ "${timestamp}" != "${lastest}" ]; then
+    echo ${LIST_NAME} ${count}
+  fi
+  lastest=${timestamp}
+  sleep 0.1
 done <${WORD_SOURCE}
