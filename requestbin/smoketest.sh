1d65b681f8b7f0abe4ebe70ca6d176347f9b29a9
crccheck <c@crccheck.com>
2016-08-02 13:40:25 -0500

    tweak smoketest

--- requestbin/smoketest.sh
+++ requestbin/smoketest.sh
@@ -7,20 +7,13 @@ function lookfor {
   grep --silent "$@" && echo " . . . PASS" || echo " . . . FAIL"
 }
 
-IP=$(docker inspect --format '{{.NetworkSettings.IPAddress}}' ${container})
-# make sure we can start a docker container
-if [ -z $IP ]; then
-  echo "Docker container ${container} not currently running"
-  exit 1
-fi
-
-sleep 1
+host=localhost:8000
 
 echo -n "Homepage should load"
-\curl --silent --head $IP | lookfor "HTTP/1.1 200 OK"
+\curl --silent --head ${host} | lookfor "HTTP/1.1 200 OK"
 
 echo -n "CSS styles should load"
-\curl --silent --head $IP/static/css/bootstrap.css | lookfor "Content-Type: text/css"
+\curl --silent --head ${host}/static/css/bootstrap.css | lookfor "Content-Type: text/css"
 
 echo -n "Static files should load"
-\curl --silent --head $IP/static/img/logo-2x.png | lookfor "Content-Type: image/png"
+\curl --silent --head ${host}/static/img/logo-2x.png | lookfor "Content-Type: image/png"
